6124d332e1010ca95156db8121dd63134a1faab9
Clivaly Cardoso <clivalydev@gmail.com>
2024-05-16 07:56:20 -0300

    Projeto celular

---
+++ projeto_celular/Models/Nokia.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeto_celular.Models
+{
+    public class Nokia : Smartphone
+    {
+         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"[{nomeApp}] instalado com sucesso!");
+
+        }
+    }
+}
